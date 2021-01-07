eaed78a7dfe1471bee7304a5148d7144b7874d33
Meth Munindradasa <methsndr@gmail.com>
2021-01-07 08:22:50 +0530

    Add endpoint for React App

--- main-server/main.js
+++ main-server/main.js
@@ -1,15 +1,42 @@
+// Import dependencies
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+// Create a new express application named 'app'
 const app = express();
+
+// Set our backend port to be either an environment variable or port 8080
 const port = 8080;
 
-app.get('/',(req, res)=>{
-	res.send('hehe the site works');
+// This application level middleware prints incoming requests to the servers console, useful to see incoming requests
+app.use((req, res, next) => {
+    console.log(`Request_Endpoint: ${req.method} ${req.url}`);
+    next();
 });
 
-app.get('/mc/',(req, res)=>{
-	res.send('connect to mc.meth.dev using Minecraft! :D');
+// Configure the bodyParser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// Configure the CORs middleware
+app.use(cors());
+
+// This middleware informs the express application to serve our compiled React files
+    app.use(express.static(path.join(__dirname, 'main-front/build')));
+
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'main-front/build', 'index.html'));
+    });
+// Catch any bad requests
+app.get('*', (req, res) => {
+    res.status(200).json({
+        msg: 'Catch All'
+    });
 });
 
-app.listen(port, () => {
-	console.log(`app is listening on localhost:${port}`);
-});
+// Configure our server to listen on the port defiend by our port variable
+app.listen(port, () => console.log(`BACK_END_SERVICE_PORT: ${port}`));
