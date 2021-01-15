a42cd29ef74c681135ed0f32e4fa39d304468e02
Meth Munindradasa <methsndr@gmail.com>
2021-01-15 17:41:05 +0530

    Add colors and fonts

--- main-server/main-front/tailwind.config.js
+++ main-server/main-front/tailwind.config.js
@@ -2,7 +2,22 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "mdd-red-strong": "#FF5555",
+        "mdd-red-light": "#FF6767",
+        "mdd-green-strong": "#81FF55",
+        "mdd-green-light": "#D7FF67",
+        "mdd-blue-strong": "#55F5FF",
+        "mdd-blue-light": "#67A4FF"
+      },
+      fontFamily: {
+        'fira-code': ['Fira Code', 'monospace']
+      },
+      inset: {
+        '1/2': '50%'
+      }
+    },
   },
   variants: {
     extend: {},
