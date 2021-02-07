570656d7e0aacb4ca1b5d247b8592218c096f741
Meth Munindradasa <methsndr@gmail.com>
2021-02-07 02:52:53 +0000

    Fix endpoint error

--- db-api/main.js
+++ db-api/main.js
@@ -3,10 +3,10 @@ const app = express();
 
 const PORT = 8081;
 
-app.get("/test", (req, res, next) => {
+app.get("test", (req, res, next) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
 });
 
 app.listen(PORT, () => {
- console.log("Server running on port 3000");
+ console.log("Server running on port 8081");
 });
