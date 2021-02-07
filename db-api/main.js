67b6216cdec28adf64ca9bb9fd5813f7d44dff7d
Meth Munindradasa <methsndr@gmail.com>
2021-02-07 02:57:44 +0000

    another endpoint bug

--- db-api/main.js
+++ db-api/main.js
@@ -3,7 +3,7 @@ const app = express();
 
 const PORT = 8081;
 
-app.get("test", (req, res, next) => {
+app.get("/test", (req, res, next) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
 });
 
