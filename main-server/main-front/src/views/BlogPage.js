239002ad78ab89bea1ed395c5010eabcd3ad07b0
Meth Munindradasa <methsndr@gmail.com>
2021-04-09 13:06:26 +0530

    Read data test

--- main-server/main-front/src/views/BlogPage.js
+++ main-server/main-front/src/views/BlogPage.js
@@ -1,6 +1,16 @@
+import {db} from '../utils/fb';
 import Header from "../widgets/Header";
 
 function BlogPage(){
+
+  let blogRef = db.collection("blog_posts");
+
+  blogRef.get()
+    .then(querySnapshot=>{
+      querySnapshot.forEach(doc=>console.log(doc.data()));
+    })
+    .catch(e=>console.log(e));
+
   let elem = (
     <div className="main-bg-div">
       <Header />
