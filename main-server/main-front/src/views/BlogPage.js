c0c1c40edfb78b8bf8ae25af6e461812a93a8b17
Meth Munindradasa <methsndr@gmail.com>
2021-04-09 12:26:38 +0530

    Add header to all pages

--- main-server/main-front/src/views/BlogPage.js
+++ main-server/main-front/src/views/BlogPage.js
@@ -1,6 +1,9 @@
+import Header from "../widgets/Header";
+
 function BlogPage(){
   let elem = (
     <div className="main-bg-div">
+      <Header />
       <div className="general-text">Bless me with the sweet release of death</div>
     </div>
   );
--- main-server/main-front/src/views/ProjectsPage.js
+++ main-server/main-front/src/views/ProjectsPage.js
@@ -1,6 +1,9 @@
+import Header from "../widgets/Header";
+
 function ProjectsPage(){
   let elem = (
     <div className="main-bg-div">
+      <Header />
       <div className="general-text">illuminati confirmed</div>
     </div>
   );
