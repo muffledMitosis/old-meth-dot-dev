a5499face03427bafdc98849522e2dbbfa3988aa
Meth Munindradasa <methsndr@gmail.com>
2021-04-10 15:34:51 +0530

    Fix header alignment issues

--- main-server/main-front/src/widgets/Header.js
+++ main-server/main-front/src/widgets/Header.js
@@ -7,12 +7,14 @@ let sections = ["Projects", "Blog", "About"];
 
 function Header() {
   let elem = (
-    <div className="relative flex flex-row space-x-4 items-center float-right mr-3 mt-3">
-      {sections.map(section => <LandingSection name={section} />)}
-      <div className="flex flex-row text-lg text-white space-x-3 mr-4">
-        <FaInstagram className="cursor-pointer" onClick={() => window.location.href = "https://www.instagram.com/muffledmitosis/"} />
-        <FiTwitter className="cursor-pointer" />
-        <FiGithub className="cursor-pointer" onClick={() => window.location.href = "https://github.com/muffledMitosis"} />
+    <div className="">
+      <div className="flex flex-row space-x-4 items-center justify-end mr-3 mt-3">
+        {sections.map(section => <LandingSection name={section} />)}
+        <div className="flex flex-row text-lg text-white space-x-3 mr-4">
+          <FaInstagram className="cursor-pointer" onClick={() => window.location.href = "https://www.instagram.com/muffledmitosis/"} />
+          <FiTwitter className="cursor-pointer" />
+          <FiGithub className="cursor-pointer" onClick={() => window.location.href = "https://github.com/muffledMitosis"} />
+        </div>
       </div>
     </div>
   );
