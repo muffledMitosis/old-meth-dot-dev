e6f4ad66f1c3cc6ee2ff3221388604c221b1aa6b
Meth Munindradasa <methsndr@gmail.com>
2021-04-13 04:05:22 +0530

    Add proper date display

--- main-server/main-front/src/widgets/BlogCard.js
+++ main-server/main-front/src/widgets/BlogCard.js
@@ -1,12 +1,14 @@
 function BlogCard(props){
 
   // TODO: Add proper styles
+  let timestamp = props.data["timestamp"].toDate();
+
   let elem = (
-    <div className="w-8/12 cursor-pointer">
-      <img className="pl-4 mb-6 w-100" src={props.data["imgUrl"]} />
+    <div className="w-8/12 cursor-pointer pl-10">
+      <img className="pl-6 mb-6 w-100" src={props.data["imgUrl"]} />
       <div className="card-h font-light text-2xl">{props.data["mainHeader"]}</div>
-      <div>{String(props.data["timestamp"])}</div>
-      <div className="general-text font-thin text-xs">{props.data["introText"]}</div>
+      <div className="general-text text-xs pl-1 my-2">{"Meth Munindradasa - " + timestamp.getFullYear() + '/' + (timestamp.getMonth()+1) + '/' + timestamp.getDate()}</div>
+      <div className="general-text font-thin text-sm">{props.data["introText"]}</div>
     </div>
   );
 
