88e21e7d40b906e2d844e0cef9d5bff151a33c42
Meth Munindradasa <methsndr@gmail.com>
2021-04-13 04:57:00 +0530

    Add base markdown functionality

--- main-server/main-front/src/views/BlogReader.js
+++ main-server/main-front/src/views/BlogReader.js
@@ -1,24 +1,24 @@
+import React from 'react'
+import { render } from '@testing-library/react';
+import ReactMarkdown from 'react-markdown';
 import { useHistory, useParams } from 'react-router-dom'
 import Header from '../widgets/Header';
 
 // TODO: add markdown parser to display claps and stuff
 
 function RenderBlog(data){
-  let elem = (
-    <div>
-
-    </div>
-  );
-
-  return elem;
+  let dt = "# lmao, *lmao*";
+  return <ReactMarkdown>{dt}</ReactMarkdown>
 }
 
 function BlogReader(){
 
   const {id} = useParams();
 
+  // RenderBlog(id);
+
   let elem = (
-    <div className="main-bg-div general-text">
+    <div className="main-bg-div">
       <Header />
       {RenderBlog(id)}
     </div>
