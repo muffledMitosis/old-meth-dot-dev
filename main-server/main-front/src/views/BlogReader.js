0648c32aac946fefd08eb1650a4f3c11b02522fa
Meth Munindradasa <methsndr@gmail.com>
2021-04-17 21:07:04 +0530

    Start experementing with markdown styles

--- main-server/main-front/src/views/BlogReader.js
+++ main-server/main-front/src/views/BlogReader.js
@@ -1,26 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { render } from '@testing-library/react';
 import ReactMarkdown from 'react-markdown';
 import { useHistory, useParams } from 'react-router-dom'
 import Header from '../widgets/Header';
+import { db } from '../utils/fb';
 
 // TODO: add markdown parser to display claps and stuff
 
 function RenderBlog(data){
-  let dt = "# lmao, *lmao*";
-  return <ReactMarkdown>{dt}</ReactMarkdown>
+  console.log(data);
+
+  let dt = `
+  # This is a H1
+  ## This is a H2 
+  ### This is a H3
+
+  some paragraph trying to *explain* stuff **pretty important** do line breaks work? guess so lmao
+
+  ## New topic
+  explenation of new topic lmao
+
+
+
+  * Lists
+  * [ ] todo
+  * [x] done
+  
+  `;
+  return <ReactMarkdown className="md-view">{dt}</ReactMarkdown>
 }
 
 function BlogReader(){
 
   const {id} = useParams();
+  const [blogData, setBlogData] = useState(false);
 
-  // RenderBlog(id);
+  let blogRef = db.collection("blog_posts").doc(String(id));
+
+  if(!blogData){
+    blogRef.get()
+    .then(doc=>{
+      setBlogData(doc.data());
+    }).catch(e=>console.log(e));
+  }
 
   let elem = (
     <div className="main-bg-div">
       <Header />
-      {RenderBlog(id)}
+      {RenderBlog(blogData)}
     </div>
   );
 
