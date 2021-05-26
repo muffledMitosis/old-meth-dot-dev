ff3030159a2b1049d6da5f9b37efee66822a0cfe
Meth Munindradasa <methsndr@gmail.com>
2021-05-26 08:39:01 +0530

    Fixed this bigarse bug lmao dunno how it got there

--- firebase/main-front/src/views/BlogReader.js
+++ firebase/main-front/src/views/BlogReader.js
@@ -12,20 +12,7 @@ import { text } from 'body-parser';
 // display claps and stuff
 
 function RenderBlog(data){
-  console.log(data);
-
-  const components = {
-    code({node, inline, className, children, ...props}) {
-      const match = /language-(\w+)/.exec(className || '')
-      return !inline && match ? (
-        <SyntaxHighlighter style={dark} language={match[1]} PreTag="div" children={String(children).replace(/\n$/, '')} {...props} />
-      ) : (
-        <code className={className} {...props} />
-      )
-    }
-  }
-
-  return <ReactMarkdown components={components} className="md-view" plugins={[gfm]}>{data}</ReactMarkdown>
+  return <ReactMarkdown className="md-view" plugins={[gfm]}>{data}</ReactMarkdown>
 }
 
 function BlogReader(){
@@ -33,18 +20,15 @@ function BlogReader(){
   const {id} = useParams();
   const [blogData, setBlogData] = useState(false);
 
-  let blogRef = db.collection("blog_posts").doc(String(id));
+  let blogRef = db.collection("blog").doc(String(id));
 
   if(!blogData){
     blogRef.get()
     .then(doc=>{
-      console.log(doc.data()["contentBlobLocation"]);
-      
       var xhr = new XMLHttpRequest();
       xhr.responseType = 'text'; 
       xhr.onload = function(event) {
         var md_text= xhr.response;
-        console.log(md_text);      // now you read the file content
         setBlogData(md_text);
       };
       xhr.open('GET', doc.data()["contentBlobLocation"]);
