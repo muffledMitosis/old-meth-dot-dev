5876060238131f21d099d681d7bb10f2b7dd9664
Meth Munindradasa <methsndr@gmail.com>
2021-07-17 17:31:15 +0530

    Add image support

--- gatsby/mdd/gatsby-config.js
+++ gatsby/mdd/gatsby-config.js
@@ -21,6 +21,9 @@ module.exports = {
     ]
   },
   plugins: [
-    'gatsby-plugin-postcss'
+    'gatsby-plugin-postcss',
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
   ],
 };
