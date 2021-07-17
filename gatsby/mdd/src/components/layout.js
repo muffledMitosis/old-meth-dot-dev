f18a48df9ee1a29c291819536f4dbcd7503686f6
Meth Munindradasa <methsndr@gmail.com>
2021-07-17 17:31:32 +0530

    Complete landing page

--- gatsby/mdd/src/components/layout.js
+++ gatsby/mdd/src/components/layout.js
@@ -1,6 +1,7 @@
 import { graphql, StaticQuery } from 'gatsby';
 import * as React from 'react';
 import NavClickable from './widgets/nav_clickable';
+import {FiTwitter, FiGithub, FiInstagram} from 'react-icons/fi';
 
 const Layout = ({children}) => {
 
@@ -22,7 +23,15 @@ const Layout = ({children}) => {
 		<main>
 			<StaticQuery query={nav_querry} render={data => (
 				<React.Fragment>
-					<div className="flex flex-row space-x-4 items-center justify-end mr-3 -mt-3">{data.site.siteMetadata.navbarInfo.map(item => <li><NavClickable name={item.name} onClickLocation={item.onClickLocation} color={item.color}/></li>)}</div>
+					<div className="flex flex-row space-x-4 items-center justify-end mr-3 -mt-3">
+						{data.site.siteMetadata.navbarInfo.map(item => <li><NavClickable name={item.name} onClickLocation={item.onClickLocation} color={item.color}/></li>)}
+
+						<div className="flex flex-row text-lg text-white space-x-3 mr-4 mt-6">
+							<FiInstagram className="cursor-pointer" onClick={() => window.location.href = "https://instagram.com/muffledmitosis"}/>
+							<FiTwitter className="cursor-pointer" />
+							<FiGithub className="cursor-pointer" onClick={() => window.location.href = "https://github.com/muffledMitosis"} />
+        		</div>
+					</div>
 				</React.Fragment>
 			)} />
 			{children}
--- gatsby/mdd/src/pages/index.js
+++ gatsby/mdd/src/pages/index.js
@@ -1,3 +1,4 @@
+import { StaticImage } from 'gatsby-plugin-image';
 import * as React from 'react';
 import Layout from '../components/layout';
 
@@ -5,7 +6,12 @@ const IndexPage = () => {
   return(
     <Layout>
       <div className="h-screen">
-        <h1 className="general-text">lmao</h1>
+        <StaticImage className="absolute right-0 bottom-0 w-1/2 md:w-1/4 pointer-events-none" src="../assets/art/tl-art.svg" />
+
+            <div className="text-white font-fira-code absolute left-8 md:left-32 top-1/2 transform -translate-y-full md:-translate-y-3/4">
+                <div className="flex flex-row items-center space-x-2"><StaticImage className="w-10 h-10 md:w-24 md:h-24 object-cover rounded-full" src="../assets/images/profilepic.png" /><div className="text-lg md:text-6xl">Hello There 👋</div></div>
+                <div className="text-xl md:text-4xl mt-4">I’m Meth, a full stack web dev<br />Works remotely, currently in SL</div>
+            </div>
       </div>
     </Layout>
   );
