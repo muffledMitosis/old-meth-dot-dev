b0406387fef199214a062aeb1ab6adfcc660b688
Meth Munindradasa <methsndr@gmail.com>
2021-07-23 12:45:48 +0530

    Proper styling for Project Card

--- gatsby/mdd/src/components/widgets/project_card.js
+++ gatsby/mdd/src/components/widgets/project_card.js
@@ -3,10 +3,13 @@ import Img from "gatsby-image";
 
 const ProjectCard = ({gotoLink, img, projectName, introText}) => {
   return(
-	<div className="w-8/12 cursor-pointer pl-10" onClick={()=>window.location.href=gotoLink}>
-		<Img className="w-100" fluid={img} />
-		{/* <div className="card-h font-light text-2xl">{projectName}</div>
-		<div className="general-text font-thin text-sm">{introText}</div> */}
+	<div className="w-100 cursor-pointer relative" onClick={()=>window.location.href=gotoLink}>
+		<div className=""><Img className="w-100" fluid={img} /></div>
+		<div className="project-card-gradient"></div>
+		<div className="absolute z-50 bottom-0 project-card-text ml-4 mb-2">
+			<div className="card-h font-light text-2xl">{projectName}</div>
+			<div className="general-text font-thin text-sm">{introText}</div>
+		</div>
 	</div>
   );
 };
