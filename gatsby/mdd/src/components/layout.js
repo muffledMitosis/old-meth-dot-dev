520953c71fef63b853896133e7b6c02cc0aef100
Meth Munindradasa <methsndr@gmail.com>
2021-08-04 07:41:26 +0530

    Fix alignment issues

--- gatsby/mdd/src/components/layout.js
+++ gatsby/mdd/src/components/layout.js
@@ -23,9 +23,8 @@ const Layout = ({children}) => {
 		<main>
 			<StaticQuery query={nav_querry} render={data => (
 				<React.Fragment>
-					<div className="flex flex-row space-x-4 items-center justify-end mr-3 -mt-3">
+					<div className="flex flex-row space-x-4 items-center justify-end mr-6 -mt-3">
 						{data.site.siteMetadata.navbarInfo.map(item => <li><NavClickable name={item.name} onClickLocation={item.onClickLocation} color={item.color}/></li>)}
-
 						<div className="flex flex-row text-lg text-white space-x-3 mr-4 mt-6">
 							<FiInstagram className="cursor-pointer" onClick={() => window.location.href = "https://instagram.com/muffledmitosis"}/>
 							<FiTwitter className="cursor-pointer" onClick={() => window.location.href = "https://twitter.com/muffledmitosis"} />
