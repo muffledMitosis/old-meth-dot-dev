a756717a8c7a32f4b90f7cd01a9033099141cb03
Meth Munindradasa <methsndr@gmail.com>
2021-08-09 07:58:48 +0530

    Working skeleton of responsive navbar

--- gatsby/mdd/src/components/layout.js
+++ gatsby/mdd/src/components/layout.js
@@ -19,18 +19,27 @@ const Layout = ({children}) => {
 }
 `;
 
+	const [navBarState, setNavBarState] = React.useState(true);
+	
+	const navBarFunc = (data) => (
+		<div className="absolute right-0 bg-mdd-purple-dark z-50 md:bg-opacity-0 md:static md:flex md:flex-row md:space-x-4 md:items-center md:justify-end md:mr-6 md:mt-3">
+			<div className="text-white cursor-pointer md:hidden" onClick={() => setNavBarState(!navBarState)}>HAM</div>
+			<div className={(navBarState ? "hidden " : "block ") + "md:block md:flex md:flex-row md:space-x-4 md:items-center md:justify-end"}>
+				{data.site.siteMetadata.navbarInfo.map(item => <div><NavClickable name={item.name} onClickLocation={item.onClickLocation} color={item.color}/></div>)}
+				<div x-show="open" className="flex flex-row text-lg text-white space-x-3 mr-4">
+					<FiInstagram className="cursor-pointer" onClick={() => window.location.href = "https://instagram.com/muffledmitosis"}/>
+					<FiTwitter className="cursor-pointer" onClick={() => window.location.href = "https://twitter.com/muffledmitosis"} />
+					<FiGithub className="cursor-pointer" onClick={() => window.location.href = "https://github.com/muffledMitosis"} />
+				</div>
+			</div>
+		</div>
+	)
+
 	return (
-		<main>
+		<main className="">
 			<StaticQuery query={nav_querry} render={data => (
 				<React.Fragment>
-					<div className="flex flex-row space-x-4 items-center justify-end mr-6 -mt-3">
-						{data.site.siteMetadata.navbarInfo.map(item => <li><NavClickable name={item.name} onClickLocation={item.onClickLocation} color={item.color}/></li>)}
-						<div className="flex flex-row text-lg text-white space-x-3 mr-4 mt-6">
-							<FiInstagram className="cursor-pointer" onClick={() => window.location.href = "https://instagram.com/muffledmitosis"}/>
-							<FiTwitter className="cursor-pointer" onClick={() => window.location.href = "https://twitter.com/muffledmitosis"} />
-							<FiGithub className="cursor-pointer" onClick={() => window.location.href = "https://github.com/muffledMitosis"} />
-        		</div>
-					</div>
+					{navBarFunc(data)}
 				</React.Fragment>
 			)} />
 			{children}
