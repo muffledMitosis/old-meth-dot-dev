65377715e695ede59cb53562d0ecbc2880b26413
Meth Munindradasa <methsndr@gmail.com>
2021-08-09 17:58:58 +0530

    Fix color persistance bug

--- gatsby/mdd/src/components/layout.js
+++ gatsby/mdd/src/components/layout.js
@@ -23,9 +23,9 @@ const Layout = ({children}) => {
 	const [navBarState, setNavBarState] = React.useState(true);
 	
 	const navBarFunc = (data) => (
-		<div className={(navBarState ? "bg-none " : "bg-mdd-purple-dark pl-4 main-bg-gradient-inverted ") + "absolute h-full pr-4 pt-4 space-y-4 right-0 z-50 md:bg-opacity-0 md:static md:flex md:flex-row md:space-x-4 md:items-center md:justify-end md:mr-6 md:mt-3"}>
+		<div className={(navBarState ? "bg-none clearGradient " : "pl-4 main-bg-gradient-inverted ") + "clearGradient absolute h-full pr-4 pt-4 space-y-4 right-0 z-50 md:bg-opacity-0 md:static md:flex md:flex-row md:space-x-4 md:items-center md:justify-end md:mr-6 md:pt-0"}>
 			<div className="text-white cursor-pointer md:hidden mb-4" onClick={() => setNavBarState(!navBarState)}><GiHamburgerMenu className="float-right mb-4 text-xl"/></div>
-			<div className={(navBarState ? "hidden " : "block ") + "md:flex md:flex-row md:space-x-4 md:items-center md:justify-end navMenu space-y-2 pt-4 md:space-y-0 md:pt-0"}>
+			<div className={(navBarState ? "hidden " : "block ") + "md:flex md:flex-row md:space-x-4 md:items-center md:justify-end navMenu space-y-2 md:space-y-0 md:pt-0 pt-4"}>
 				{data.site.siteMetadata.navbarInfo.map(item => <div><NavClickable name={item.name} onClickLocation={item.onClickLocation} color={item.color}/></div>)}
 				<div x-show="open" className="flex flex-row text-lg text-white space-x-3 mr-4">
 					<FiInstagram className="cursor-pointer" onClick={() => window.location.href = "https://instagram.com/muffledmitosis"}/>
