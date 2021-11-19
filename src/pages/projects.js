b691767a37c799fdbd63c50185b93fa71ef99619
Meth Munindradasa <methsndr@gmail.com>
2021-11-19 10:21:00 +0530

    Quick fix for Projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,17 +1,21 @@
 import {graphql} from 'gatsby';
 import * as React from 'react';
+import {GiHammerDrop} from 'react-icons/gi';
 import Layout from '../components/layout';
 import ProjectCard from '../components/widgets/project_card';
 
 const ProjectsPage = ({data: {allMarkdownRemark: {edges}}}) => {
 
   const ProjC = (edge) => {
-    return <ProjectCard
-			gotoLink={edge.node.frontmatter.slug}
-			img={edge.node.frontmatter.imageLocation.childImageSharp.fluid}
-			projectName={edge.node.frontmatter.title}
-			introText={edge.node.frontmatter.intro}
-		/>
+  //   return <ProjectCard
+		// 	gotoLink={edge.node.frontmatter.slug}
+		// 	img={edge.node.frontmatter.imageLocation.childImageSharp.fluid}
+		// 	projectName={edge.node.frontmatter.title}
+		// 	introText={edge.node.frontmatter.intro}
+		// />
+		return <div className="text-mdd-green-light text-2xl ml-2">
+			<GiHammerDrop className="inline text-mdd-red-light" /> WORK IN PROGRESS
+			</div>
   }
 
   const posts = edges
