4dcc4bb13dadaabd9c4e3ffe3ffb161110c52f9b
Meth Munindradasa <methsndr@gmail.com>
2021-12-25 14:21:17 +0530

    Add tech stack

--- src/pages/about.js
+++ src/pages/about.js
@@ -33,15 +33,68 @@ const MainText = () => {
   );
 }
 
+const FrontEnd =()=>{
+  return(
+    <div className='flex items-center'>
+      <div className='w-16 ml-2 py-2'><StaticImage src='../assets/art/tech_logos/react.svg'/></div>
+      <div className='w-24 ml-2 py-2'><StaticImage src='../assets/art/tech_logos/tailwindcss.svg'/></div>
+    </div>
+  )
+}
+
+const Server =()=>{
+  return(
+    <div className='flex items-center'>
+      <div className='w-16 ml-2 py-2'><StaticImage src='../assets/art/tech_logos/nodejs.svg'/></div>
+      <div className='w-16 ml-2 py-2'><StaticImage src='../assets/art/tech_logos/firebase.svg'/></div>
+    </div>
+  )
+}
+const DB =()=>{
+  return(
+    <div className='flex items-center'>
+      <div className='w-24 ml-2 py-2'><StaticImage src='../assets/art/tech_logos/mongodb.svg'/></div>
+      <div className='w-16 ml-2 py-2'><StaticImage src='../assets/art/tech_logos/postgresql.svg'/></div>
+      <div className='w-16 ml-2 py-2'><StaticImage src='../assets/art/tech_logos/firebase.svg'/></div>
+    </div>
+  )
+}
+const TechStack = () => {
+  return(
+    <div className='ml-8 md:ml-24'>
+      <table className='table-auto general-text'>
+        <thead>
+          <tr><th></th><th><div className='m-2'>Technologies</div></th></tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td><div className='m-2'>Frontend</div></td>
+            <td><FrontEnd/></td>
+          </tr>
+          <tr>
+            <td><div className='m-2'>DB</div></td>
+            <td><DB/></td>
+          </tr>
+          <tr>
+            <td><div className='m-2'>Server</div></td>
+            <td><Server/></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
 const AboutPage = () => {
   return(
     <Layout>
       <div className="min-h-screen">
-        {/* <TechScroll /> */}
         <div className="flex md:flex-row flex-col items-center md:items-start pt-4">
           <PictureFrame className="ml-0 mb-2 md:ml-24 mr-0 md:mr-8 mt-8 md:mt-0" />
           <MainText />
         </div>
+        <div className="text-3xl md:text-4xl general-text mb-8 ml-8 md:ml-24 mt-16">My Goto Tech-Stack</div>
+        <TechStack />
       </div>
     </Layout>
   );
