8f35d7a1869f36166cb19f73e6423e170b944f24
Meth Munindradasa <methsndr@gmail.com>
2021-12-25 08:15:06 +0530

    Add image for about page. Fix alignment

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,4 @@
+import { StaticImage } from 'gatsby-plugin-image';
 import * as React from 'react';
 import { FiCpu, FiServer, FiTool } from 'react-icons/fi';
 import Layout from '../components/layout';
@@ -9,20 +10,20 @@ const Tool_i = () => <FiTool className="inline" />
 
 const PictureFrame = (props) => {
   return(
-    <div className={"about-picture-frame bg-green-400 " + props.className}>
-
+    <div className={props.className}>
+      <StaticImage className='w-64' src="../assets/images/about_pic.JPG"/>
     </div>
   );
 }
 const MainText = () => {
   return(
-    <div className="about-main-text">
-      <div className="general-text text-2xl intro">
-        <div>👋 Hi there, I'm</div>
-        <div className="text-4xl">Meth <b>Munindradasa</b></div>
+    <div className="about-main-text ml-4 mt-2 md:mt-0">
+      <div className="general-text text-xl md:text-2xl intro">
+        {/* <div>👋 Hi there, I'm</div> */}
+        <div className="text-3xl md:text-4xl">Meth <b>Munindradasa</b></div>
         <div>A Full Stack Web Dev</div>
       </div>
-      <div className="general-text description mt-4">
+      <div className="general-text text-sm md:text-base description mt-4">
         I specialize in   <Sever_i /> <b>Backend Development with NodeJS</b>,   <Cpu_i /> <b>API architecture </b>
         and   <Tool_i /> <b>Tools/Plugin Development</b>. I have a deep passion for electronics
         and computer graphics, the two in combination is just amazing and fun to
@@ -38,7 +39,7 @@ const AboutPage = () => {
       <div className="min-h-screen">
         {/* <TechScroll /> */}
         <div className="flex md:flex-row flex-col items-center md:items-start pt-4">
-          <PictureFrame className="ml-0 md:ml-24 mr-0 md:mr-8" />
+          <PictureFrame className="ml-0 mb-2 md:ml-24 mr-0 md:mr-8 mt-8 md:mt-0" />
           <MainText />
         </div>
       </div>
